60045aefef4432b15bee19e552e5ec303e986edf
Kyrllan <kyrllan@hotmail.com>
2024-01-31 09:52:07 -0300

    fixed users seed

--- database/seeders/FirstUser.ts
+++ database/seeders/FirstUser.ts
@@ -5,12 +5,28 @@ export default class FirstUserSeeder extends BaseSeeder {
   public async run() {
     await User.createMany([
       {
-        name: "John Doe",
-        email: "johndoe@gmail.com",
-        password: "123456",
+        name: "John Doe (Admin)",
+        email: "admin@gmail.com",
+        password: "doe",
         cpf: "123.456.789-10",
         phone: "(11) 12345-6789",
         role: "admin",
+      },
+      {
+        name: "Barbeiro 1",
+        email: "barber@gmail.com",
+        password: "123456",
+        cpf: "123.456.789-10",
+        phone: "(11) 12345-6789",
+        role: "employee",
+      },
+      {
+        name: "Barbeiro 2",
+        email: "barber2@gmail.com",
+        password: "123456",
+        cpf: "123.456.789-10",
+        phone: "(11) 12345-6789",
+        role: "employee",
       },
       {
         name: "Cliente 1",
@@ -21,14 +37,21 @@ export default class FirstUserSeeder extends BaseSeeder {
         role: "user",
       },
       {
-        name: "Barbeiro 1",
-        email: "barber@gmail.com",
+        name: "Cliente 2",
+        email: "cliente2@gmail.com",
         password: "123456",
         cpf: "123.456.789-10",
         phone: "(11) 12345-6789",
-        role: "employee",
+        role: "user",
+      },
+      {
+        name: "Cliente 3",
+        email: "cliente3@gmail.com",
+        password: "123456",
+        cpf: "123.456.789-10",
+        phone: "(11) 12345-6789",
+        role: "user",
       },
     ]);
-    // Write your database queries inside the run method
   }
 }
